2c6ffdf732b8b9c6cffc4f75b28e679563b0a4dd
emmamuwanguzi <emmuw23@gmail.com>
2023-07-27 01:44:17 -0700

    Add project files.

---
+++ analogcircuitry/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analogcircuitry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            components strut1 = new components();
+            Console.WriteLine("Enter the components");
+            Console.WriteLine(" components1");
+            strut1.Component1=Console.ReadLine();
+            Console.WriteLine("components2");
+            strut1.Component2 = Console.ReadLine();
+            //Console.WriteLine($"role {strut1.Role}");
+            strut1.identify();
+            Console.WriteLine($"role {strut1.role}");
+            
+            strut1.problemsFacedByDesigners();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ analogcircuitry/components.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace analogcircuitry
+{
+    class components
+    {
+        private string component1;
+        private string component2;
+        public string role;
+        public string Component1
+        {
+            get
+            {
+                return this.component1;
+            }
+            set
+            {
+                this.component1 = value;
+            }
+        }
+        public string Component2
+        {
+            get
+            {
+                return this.component2;
+            }
+            set
+            {
+                this.component2 = value;
+            }
+        }
+      /*  public string Role
+        {
+            get
+            {
+                return this.role;
+            }
+            set
+            {
+                this.role = value;
+            }
+        }*/
+        public components()
+        {
+            this.component1 = "power source";
+            this.component2 = " rectifier";
+            this.role = " ac to dc";
+        }
+        public components(string component1,string component2,string role)
+        {
+            this.component1 = component1;
+            this.component2 = component2;
+            this.role = role;
+        }
+        public void problemsFacedByDesigners()
+        {
+            Console.WriteLine("Insatbility and drift due to temperature variations\nDynamic range of signals" +
+                "and non linearity when pressing the limits of the range\n" +
+                "Inaccuracies of computation when using analog quantities\n" +
+                "Adequate signal frequency range");
+        }
+        public void identify()
+        {
+            if (component2 == "inverter")
+                role = "dc to ac";
+            else
+                role = " ac to dc";
+                    
+        }
+    }
+}
